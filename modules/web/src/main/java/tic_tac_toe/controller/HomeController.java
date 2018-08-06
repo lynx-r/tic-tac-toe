701d6f2448e8b4e1103902994a522c88ef5e3d83
vladimir.kurov <vladimir.kurov@simbirsoft.com>
2018-08-07 02:09:00 +0400

    add postgresql

--- modules/web/src/main/java/tic_tac_toe/controller/HomeController.java
+++ modules/web/src/main/java/tic_tac_toe/controller/HomeController.java
@@ -1,16 +1,14 @@
 package tic_tac_toe.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
-@RestController
-@RequestMapping("/")
+@Controller
 public class HomeController {
 
-    @GetMapping
+    @RequestMapping("/")
     public String hello() {
         return "HELLO";
     }
