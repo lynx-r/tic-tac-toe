16a3e1dca1a3a0b30c646356ae9d873999065e87
vladimir.kurov <vladimir.kurov@simbirsoft.com>
2018-08-21 22:12:42 +0400

    add options

--- modules/web/src/main/java/tic_tac_toe/controller/GameStartController.java
+++ modules/web/src/main/java/tic_tac_toe/controller/GameStartController.java
@@ -2,11 +2,13 @@ package tic_tac_toe.controller;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import tic_tac_toe.GameStartService;
+import tic_tac_toe.GameStartServiceImpl;
 import tic_tac_toe.config.Urls.Game.GameStart;
 import tic_tac_toe.domain.game.start.GameStartRequest;
 import tic_tac_toe.domain.game.start.GameStartResponse;
@@ -18,6 +20,7 @@ import tic_tac_toe.domain.game.start.GameStartResponse;
 public class GameStartController {
     private final GameStartService gameStartService;
 
+    @CrossOrigin
     @PostMapping
     public GameStartResponse startGame(@RequestBody GameStartRequest request) {
         return gameStartService.startGame(request);
